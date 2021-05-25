ed68aa4b1e9ce159a2f23d0db97eca3558f00ebf
rodolfo_oliveira <rod.vgo@gmail.com>
2021-05-24 23:40:23 -0300

    criando rotas 2

--- EXPRESS/index.js
+++ EXPRESS/index.js
@@ -6,12 +6,22 @@ app.get('/', (req, res) => {
   res.send('Bem vindo ao Express');
 });
 
-app.get('/blog', (req, res) => {
-  res.send('Bem vindo ao blog');
+app.get('/blog/:artigo?', (req, res) => {
+  const { artigo } = req.params;
+  if (artigo) {
+    res.send(`<h2>Artigo: ${artigo} </h2>`);
+  } else {
+    res.send('Bem vindo ao blog');
+  }
 });
 
 app.get('/canal/youtube', (req, res) => {
   res.send('<h1>Bem vindo ao meu canal</h1>');
+});
+
+app.get('/ola/:nome/:empresa', (req, res) => {
+  const { nome, empresa } = req.params;
+  res.send(`<h1>Oi ${nome} ${empresa}</h1>`);
 });
 
 app.listen(4000, (erro) => {
