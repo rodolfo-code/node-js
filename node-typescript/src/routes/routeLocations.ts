f43510345ca94285b3d15dbd7411d0b852879192
rodolfo_oliveira <rod.vgo@gmail.com>
2021-08-25 08:17:41 -0300

    encontrar por id

--- node-typescript/src/routes/routeLocations.ts
+++ node-typescript/src/routes/routeLocations.ts
@@ -28,23 +28,50 @@ locationsRouter.post('/', async (req, res) => {
     uf
   }
 
+  const transaction = await connection.transaction()
 
-  const newIds = await connection('locations').insert(location)
-  const locationId = newIds[0];
+  const newIds = await transaction('locations').insert(location)
+  const location_id = newIds[0];
 
-  const locationItems = items.map((item_Id: Number) => {
+  const locationItems = items.map((item_id: Number) => {
+    // transaction('items').where('id', item_id).first()
+    //   .then(data => {
+    //     if (!data) {
+    //       return res.status(400).json({message: 'Item not found'})
+    //     }
+    //   })
+
     return {
-      item_Id,
-      location_id: locationId
+      item_id,
+      location_id
     }
   })
 
-  await connection('location_items').insert(locationItems)
+  await transaction('location_items').insert(locationItems)
+
+  await transaction.commit()
 
   return res.json({
-    id: locationId,
+    id: location_id,
     ...location
   })
+})
+
+locationsRouter.get('/:id', async (req, res) => {
+  const {id} = req.params;
+
+  const location = await connection('locations').where('id', id).first()
+
+  if (!location) {
+    return res.status(400).json({message: 'Location not found'})
+  }
+
+  const items = await connection('items')
+    .join('location_items', 'items.id', '=', 'location_items.item_id' )
+    .where('location_items.location_id', id)
+    .select('items.title')
+
+  return res.json({location, items})
 })
 
 export default locationsRouter;
