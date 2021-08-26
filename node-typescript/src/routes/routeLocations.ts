12d80755e20ee2f53d9046314fd42dbe2b4a27ee
rodolfo_oliveira <rod.vgo@gmail.com>
2021-08-26 09:42:29 -0300

    validaçao com celebrate

--- node-typescript/src/routes/routeLocations.ts
+++ node-typescript/src/routes/routeLocations.ts
@@ -1,10 +1,69 @@
 import { Router } from 'express'
+import multer from 'multer'
+import { celebrate, Joi } from 'celebrate'
 
-import connection from '../database/connection'
+import multerCongif from '../config/multer';
+import connection from '../database/connection';
 
 const locationsRouter = Router()
 
-locationsRouter.post('/', async (req, res) => {
+const validateData = () => celebrate( {
+  body: Joi.object().keys({
+    name: Joi.string().required(),
+    email: Joi.string().required().email(),
+    whatsapp: Joi.string().required(),
+    latitude: Joi.number().required(),
+    longitude: Joi.number().required(),
+    city: Joi.string().required(),
+    uf: Joi.string().required().max(2),
+    items: Joi.string().required(),
+})
+}, {
+  abortEarly: false,
+})
+
+const upload = multer(multerCongif)
+
+locationsRouter.get('/', async (req, res) => {
+  const { city, uf, items } = req.query;
+
+  if (city && uf && items) {
+    const parsedItems: Number[] = String(items).split(',').map(item => Number(item.trim()));
+
+    const locations = await connection('locations')
+        .join('location_items', 'locations.id', '=', 'location_items.location_id')
+        .whereIn('location_items.item_id', parsedItems)
+        .where('city', String(city))
+        .where('uf', String(uf))
+        .distinct()
+        .select('locations.*',);
+
+        return res.json(locations);
+} else {
+    const locations = await connection('locations').select('*');
+
+    return res.json(locations);
+}
+})
+
+locationsRouter.get('/:id', async (req, res) => {
+  const {id} = req.params;
+
+  const location = await connection('locations').where('id', id).first()
+
+  if (!location) {
+    return res.status(400).json({message: 'Location not found'})
+  }
+
+  const items = await connection('items')
+    .join('location_items', 'items.id', '=', 'location_items.item_id' )
+    .where('location_items.location_id', id)
+    .select('items.title')
+
+  return res.json({location, items})
+})
+
+locationsRouter.post('/', validateData(), async (req, res) => {
   console.log('ola')
   const {
     name,
@@ -57,21 +116,24 @@ locationsRouter.post('/', async (req, res) => {
   })
 })
 
-locationsRouter.get('/:id', async (req, res) => {
+locationsRouter.put('/:id', upload.single('image'), async (req, res) => {
   const {id} = req.params;
+  const image = req.file?.filename;
 
-  const location = await connection('locations').where('id', id).first()
+  const location = await connection('locations').where('id', id).first();
 
   if (!location) {
     return res.status(400).json({message: 'Location not found'})
   }
 
-  const items = await connection('items')
-    .join('location_items', 'items.id', '=', 'location_items.item_id' )
-    .where('location_items.location_id', id)
-    .select('items.title')
+  const locationUpdated = {
+    ...location,
+    image
+  }
 
-  return res.json({location, items})
+  await connection('locations').update(locationUpdated).where('id', id)
+
+  return res.json(locationUpdated)
 })
 
 export default locationsRouter;
--- node-typescript/src/server.ts
+++ node-typescript/src/server.ts
@@ -1,5 +1,7 @@
 import express from 'express'
+import cors from 'cors'
 import path from 'path'
+import { errors } from 'celebrate'
 import Router from './routes'
 
 // import router from './routes'
@@ -8,10 +10,18 @@ import Router from './routes'
 const app = express()
 const PORT = 3000
 
+// app.use(cors({
+//   origin: ['dominio.com.br', 'rodolfo.com.br']
+// }))
+
+app.use(cors());
+
 app.use(express.json())
 
 app.use('/items', Router.itemRouter)
 app.use('/locations', Router.locationsRouter)
+
+app.use(errors());
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
