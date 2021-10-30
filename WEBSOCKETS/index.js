a45ea7724d446928c3e076af8af1a63c35ebe1a2
rodolfo_oliveira <rod.vgo@gmail.com>
2021-10-30 12:03:19 -0300

    iniciando websocket

---
+++ WEBSOCKETS/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http);
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+http.listen(3000, () => {
+  console.log('App rodando liso');
+});
