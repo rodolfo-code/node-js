e0599b4dcb5a8b579cf17867ad5c8339b336dcb6
rodolfo_oliveira <rod.vgo@gmail.com>
2021-11-07 11:28:04 -0300

    criando entidades

---
+++ nlw-valoriza/src/database/index.ts
@@ -0,0 +1,6 @@
+import { createConnection, createConnections, getConnection } from "typeorm";
+
+createConnection();
+// createConnection("sqlite_connection")
+// getConnection("mysql_connection")
+// getConnection("sqlite_connection")
---
+++ nlw-valoriza/src/server.ts
@@ -0,0 +1,10 @@
+import "reflect-metadata"
+import express from 'express';
+
+import "./database"
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log('Server is running!')
+});
