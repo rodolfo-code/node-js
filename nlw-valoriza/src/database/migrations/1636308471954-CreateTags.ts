1a68db094b0c451cc1fabb120c8071eb4c29bf0d
rodolfo_oliveira <rod.vgo@gmail.com>
2021-11-10 00:53:06 -0300

    create tags

---
+++ nlw-valoriza/src/database/migrations/1636308471954-CreateTags.ts
@@ -0,0 +1,42 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateTags1636308471954 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "tags",
+                columns: [
+                    {
+                        // name: "id",
+                        // type: "uuid",
+                        // isPrimary: true,
+                        name: "id",
+                        type: "varchar",
+                        isPrimary: true,
+                        generationStrategy: 'uuid',
+                    },
+                    {
+                        name: "name",
+                        type: "varchar",
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()",
+                    },
+                    {
+                        name: "updated_at",
+                        type: "timestamp",
+                        default: "now()",
+                    },
+                ],
+            })
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("tags");
+    }
+
+}
