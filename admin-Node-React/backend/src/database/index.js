28cc4dc76ebc359658c1e257caae2fc6dfcba88b
rodolfo_oliveira <rod.vgo@gmail.com>
2022-01-13 21:18:29 -0300

    admin

---
+++ admin-Node-React/backend/src/database/index.js
@@ -0,0 +1,29 @@
+import Sequelize from "sequelize";
+
+import config from "../config/database";
+
+import User from "../models/user";
+
+const models = [User];
+
+class Database {
+  constructor() {
+    this.connection = new Sequelize(config);
+    this.init();
+    this.associate();
+  }
+
+  init() {
+    models.forEach((model) => model.init(this.connection));
+  }
+
+  associate() {
+    models.forEach((model) => {
+      if (model.associate) {
+        model.associate(this.connection.models);
+      }
+    });
+  }
+}
+
+export default new Database();
---
+++ admin-Node-React/backend/src/server.js
@@ -0,0 +1,24 @@
+import "dotenv/config";
+import "./database";
+
+import AdminJS from "adminjs";
+import AdminJSExpress from "@adminjs/express";
+import AdminJSSequelize from "@adminjs/sequelize";
+import express from "express";
+
+AdminJS.registerAdapter(AdminJSSequelize);
+
+const app = express();
+
+const adminJS = new AdminJS({
+  databases: [],
+  rootPath: "/admin",
+  resources: [],
+});
+
+const router = AdminJSExpress.buildRouter(adminJS);
+
+app.use(adminJS.options.rootPath, router);
+app.listen(5000, () => {
+  console.log("AdminJS is under http://localhost:500/admin");
+});
